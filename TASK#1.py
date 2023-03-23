cb9f478d3b5586acae60b85fa3753b4b53afe62d
mmnb <st.mohammad1380@gmail.com>
2023-03-23 10:35:48 +0330

    added comment to TASK#1.py and TASK#2.py. created new directory for each taks result in the format of 'results_#', instead of '#' the number of task is added

--- TASK#1.py
+++ TASK#1.py
@@ -3,9 +3,9 @@ import numpy as np
 from skimage.metrics import mean_squared_error
 
 # level numbers
-levels = [4, 8, 64, 128]
-mse = [0, 0, 0, 0]
-# newImg = []
+levels = [2, 4, 8, 64, 128]
+# list to hold mse values for each level number
+mse = [0, 0, 0, 0, 0]
 # reading image
 img = cv2.imread('./images/Lena.bmp')
 
@@ -15,11 +15,33 @@ gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 # mapping pixel values to new values but with specific number of levels
 # for example if we want 128 levels then each 2 steps in the range of 0-255
 # will be one step to achieve 128 levels
+# the following variable is for iterating over mse list
 i = 0
+# here we loop over levels list
 for level in levels:
+    # make change on the original gary scale image to create 
+    # an image with desired number of levels
+    # here we get the floor for each step
+    # for example if the value is 101 and the step is 2
+    # then the step with new number of levels should be dividable 
+    # by 2 so the new value is 100.
+    # we first divide this value by the step size, here it is 2,
+    # so we have 101 / 2 = 50.5,
+    # as it was mentioned we get the floor of this number,
+    # so we have: floor(50.0) = 50.
+    # to remove the effect of division by step size (here it's 2),
+    # we multiply the number we got by step size(2),
+    # so we have: 50 * 2 = 100.
     newImg = (np.floor(gray/(256/level))*(256/level))
+    # calculatin mean squared error using the function mentioned in the 
+    # TASK pdf
     mse[i] = mean_squared_error(gray, newImg)
     i += 1
+    # saving the result in the results_1 directory
+    # NOTE:the directory should be created before the execution of this program
+    cv2.imwrite(f"./results_1/{level}-1.jpg", newImg)
 
 # showing mean squared errors
+# as it is clear from the result the more the number of levels are the less the mse is
+# this is because the image gets closer to original image
 print(mse)
--- TASK#2.py
+++ TASK#2.py
@@ -10,14 +10,34 @@ gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
 # bits wanted 
 bits = [1, 2, 4, 6]
+# list to hold mse values for each number of bits
 mse = [0, 0, 0, 0]
 
+# here we map each value between 0-255,
+# to a value between 0-2**bit.
+# for example if the number of bit is 6,
+# we map the values in the range of [0,255],
+# to a value in the range of [0-64]
 i = 0
 for bit in bits:
+    # here we calculate the new value for each pixel
+    # for exanple consider we have a pixel with the value of 100
+    # in the range of [0, 255],
+    # find the new value, we multiply the current value bye the ratio of decrease.
+    # the ratio is calculated by dividing the new number of levels by 255
+    # for instance, we want to show each pixel by only 6 bits, so the number of
+    # levels is 2**6 = 64 levels.
+    # the ratio = 64 / 255 = 1 / 4 = 0.25
+    # so the new value for the pixel if the current value is 100 
+    # will be: 100 * 0.25 = 25
     newImg = (gray * (2**bit / 255))
-    cv2.imwrite(f"{bit}.jpg", newImg)
+    # calulating the mean squared error for each new image
     mse[i] = mean_squared_error(gray, newImg)
     i += 1
+    # saving new images to results_2 directory
+    # NOTE: the directory should be created before the execution of program
+    # othewise the results won't be saved
+    cv2.imwrite(f"./results_2/{bit}-2.jpg", newImg)
 
-
+# show mse in terminal
 print(mse)
